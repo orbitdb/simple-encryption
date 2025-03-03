c37be963de7bae4a257cdc9fbdb20c8c81874c05
Hayden Young <haydenyoung@wijiti.com>
2025-03-03 18:30:14 +0100

    chore: Use feat/encryption branch for testing.

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 import SimpleEncryption from '../src/index.js'
 
 describe('Simple Encryption', function () {
-  it('encrypts ', function () {
-    encryption = await SimpleEncryption({ password: 'hello' })
+  it('encrypts a record', async function () {
+    const encryption = await SimpleEncryption({ password: 'hello' })
   })
 })
