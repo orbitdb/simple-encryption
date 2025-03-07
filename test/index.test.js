7deac7eba59f3c05848250efa11547653bf3de88
Hayden Young <haydenyoung@wijiti.com>
2025-03-07 18:56:10 +0100

    test: Basic encrypt/decrypt test.

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,12 @@
+import { deepStrictEqual } from 'assert'
 import SimpleEncryption from '../src/index.js'
 
 describe('Simple Encryption', function () {
-  it('encrypts a record', async function () {
+  it('encrypts and decrypts some text', async function () {
+    const encoded = new TextEncoder().encode('some text')
     const encryption = await SimpleEncryption({ password: 'hello' })
+    const encrypted = await encryption.encrypt(encoded)
+    const decrypted = await encryption.decrypt(encrypted)
+    deepStrictEqual(decrypted, encoded)
   })
 })
